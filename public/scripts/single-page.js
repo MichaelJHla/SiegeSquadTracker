56017c59a9de74fb165ea3e035f565ed657674c9
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-01 14:18:34 -0800

    Display the proper page when a radio button is clicked

---
+++ public/scripts/single-page.js
@@ -0,0 +1,35 @@
+//Once the user clicks on the user settings page
+const userSettings = document.querySelector('#user-settings');
+userSettings.addEventListener('click', function() {
+    hideAll();
+    $('#user-settings-main').show();
+});
+
+//Once the user clicks on the site stats page
+const siteStats = document.querySelector('#site-stats');
+siteStats.addEventListener('click', function() {
+    hideAll();
+    $('#site-stats-main').show();
+});
+
+//Once the user clicks on the operator bans page
+const operatorBans = document.querySelector('#operator-bans');
+operatorBans.addEventListener('click', function() {
+    hideAll();
+    $('#operator-bans-main').show();
+});
+
+//Once the user clicks on the map bans page
+const mapBans = document.querySelector('#map-bans');
+mapBans.addEventListener('click', function() {
+    hideAll();
+    $('#map-bans-main').show();
+})
+
+function hideAll() {
+    $('#sign-in-main').hide();
+    $('#user-settings-main').hide();
+    $('#map-bans-main').hide();
+    $('#operator-bans-main').hide();
+    $('#site-stats-main').hide();
+}
