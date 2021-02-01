c3bdee3c779533e9bda5d24761989b6154746385
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-01 15:00:30 -0800

    Display the correct page and info depending on if the user login status

--- public/scripts/single-page.js
+++ public/scripts/single-page.js
@@ -19,6 +19,25 @@ var database = firebase.database();
 // Get a reference to the authentication service
 var auth = firebase.auth();
 
+//Get the current status of the user's login
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log("User signed in");
+        $('#sign-in-elements').hide();
+        $('#upper-elements').css('display', 'flex');
+        $('#lower-elements').css('display', 'flex');
+
+        userSettings.click();
+    } else {
+        console.log("No user signed in");
+        $('#sign-in-elements').css('display', 'flex');
+        $('#upper-elements').hide();
+        $('#lower-elements').hide();
+
+        signIn.click();
+    }
+});
+
 //Once the user clicks on the user settings page
 const userSettings = document.querySelector('#user-settings');
 userSettings.addEventListener('click', function() {
@@ -46,6 +65,13 @@ mapBans.addEventListener('click', function() {
     hideAll();
     $('#map-bans-main').show();
 });
+
+//Once the user clicks the sign-in page
+const signIn = document.querySelector('#sign-in-radio');
+signIn.addEventListener('click', function() {
+    hideAll();
+    $('#sign-in-main').show();
+})
 
 //This function hides all the elements that would appear in the main section of the page,
 // allowing for a specific page to be pulled up after all are hidden
