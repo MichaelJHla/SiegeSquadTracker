cdbb61e3c0391a8812be64437daa3ff1bc520fe1
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-01 14:23:13 -0800

    Add firebase data to the singe page script

--- public/scripts/single-page.js
+++ public/scripts/single-page.js
@@ -1,3 +1,24 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBPKECiN2v3FGsKWXxjJ_M8gFaSfHIFCs8",
+    authDomain: "siegesquadstats.firebaseapp.com",
+    databaseURL: "https://siegesquadstats.firebaseio.com",
+    projectId: "siegesquadstats",
+    storageBucket: "siegesquadstats.appspot.com",
+    messagingSenderId: "425029470216",
+    appId: "1:425029470216:web:646a68d606fbfa5bfa69bd",
+    measurementId: "G-082FGX8WQF"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Get a reference to the authentication service
+var auth = firebase.auth();
+
 //Once the user clicks on the user settings page
 const userSettings = document.querySelector('#user-settings');
 userSettings.addEventListener('click', function() {
@@ -26,6 +47,8 @@ mapBans.addEventListener('click', function() {
     $('#map-bans-main').show();
 });
 
+//This function hides all the elements that would appear in the main section of the page,
+// allowing for a specific page to be pulled up after all are hidden
 function hideAll() {
     $('#sign-in-main').hide();
     $('#user-settings-main').hide();
@@ -33,3 +56,4 @@ function hideAll() {
     $('#operator-bans-main').hide();
     $('#site-stats-main').hide();
 }
+
