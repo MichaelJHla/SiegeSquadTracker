f6fb29ea1e287b1d0dbd73ff9de762c2e85bc94c
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-04 15:32:17 -0800

    Build out functionality for the site stats page

--- public/scripts/single-page.js
+++ public/scripts/single-page.js
@@ -49,11 +49,9 @@ auth.onAuthStateChanged(user => {
             localStorage.setItem('squadname', s.val().squad);
             localStorage.setItem('username', s.val().username);
 
-            console.log("User signed in");
             //Reveal and hide the proper elements for the user's profile status
             $('#sign-in-elements').hide();
             $('#lower-elements').css('display', 'flex');
-            console.log(localStorage.getItem('username'));
             $('#user-settings-label').html('<i class="fas fa-user"></i> ' + localStorage.getItem('username'));
 
             userSettings.click(); //Click the user settings page
@@ -61,7 +59,6 @@ auth.onAuthStateChanged(user => {
     } else {
         localStorage.clear(); //Empties the local storage when no user is signed in
         
-        console.log("No user signed in");
         //Reveal and hide the proper elements for the user's profile status
         $('#sign-in-elements').css('display', 'flex');
         $('#upper-elements').hide();
@@ -110,6 +107,9 @@ userSettings.on('click', function() {
 const siteStats = $('#site-stats');
 siteStats.on('click', function() {
     hideAll();
+    $('#site-data-div').hide();
+    $('#site-selection-div').hide();
+    $('#site-stat-form').hide();
     $('#site-stats-main').show();
 });
 
@@ -638,7 +638,7 @@ operatorMapSelect.on('change', function() {
     $('#operator-display').show();
     $('#edit-operators-button').show();
 
-    database.ref("squads/" + localStorage.getItem("squadname") + "/operator-bans/" + $('#operator-map-list').val()).once('value').then(function(s) {
+    database.ref("squads/" + localStorage.getItem("squadname") + "/operator-bans/" + this.value).once('value').then(function(s) {
         //Load and display the data regarding operator bans
         $('#attack-operator').prop("src", "images/operators/" + s.val().attacker + ".svg");
         $('#defense-operator').prop("src", "images/operators/" + s.val().defender + ".svg");
@@ -684,4 +684,93 @@ submitOperatorBans.on('click', function() {
     $('#unsaved-changes').hide();
     $('#operator-display').show();
     $('#edit-operators-button').show();
+});
+
+const siteStatMapList = $('#site-stat-map-list');
+siteStatMapList.on('change', function() {
+    var map = this.value;
+
+    $('#site0').text(allSites[map][0]);
+    $('#site1').text(allSites[map][1]);
+    $('#site2').text(allSites[map][2]);
+    $('#site3').text(allSites[map][3]);
+
+    $('#site-data-div').hide();
+    $('#site-stat-form').hide();
+    $('#site-selection-div').show();
+});
+
+const siteSelection = $('#site-selection');
+siteSelection.on('change', function() {
+    $('#all-site-info').show();
+    $('#attack').prop('checked', false);
+    $('#defend').prop('checked', false);
+    $('#win').prop('checked', false);
+    $('#loss').prop('checked', false);
+    $('#yes').prop('checked', false);
+    $('#no').prop('checked', false);
+    $('#submit-div').hide();
+
+    var site = $('#site-selection').val();
+    if (site != "default") {
+        loadSiteData($('#site-stat-map-list').val(), site);
+    }
+});
+
+function loadSiteData(map, site) {
+    database.ref("squads/" + localStorage.getItem("squadname") + "/site-data/" + map + "/site" + site).once('value').then(function(s) {
+        $('#attacking-wins').text("Attacking wins: " + s.val().awin);
+        $('#attacking-losses').text("Attacking losses: " + s.val().aloss);
+
+        $('#attacking-planted-wins').text("Successful plants: " + s.val().pawin);
+        $('#attacking-planted-losses').text("Failed plants: " + s.val().paloss);
+
+        $('#defending-wins').text("Defending wins: " + s.val().dwin);
+        $('#defending-losses').text("Defending losses: " + s.val().dloss);
+
+        $('#defending-planted-wins').text("Defusers disabled: " + s.val().pdwin);
+        $('#defending-planted-losses').text("Failed disables: " + s.val().pdloss);
+
+        $('#site-data-div').show();
+        $('#site-stat-form').show();
+    });
+}
+
+const siteRadios = document.getElementsByClassName('site-radio');
+Array.from(siteRadios).forEach(function(e) {
+    e.addEventListener('click', function() {
+        if (document.querySelector('input[name="role"]:checked') != null && document.querySelector('input[name="success"]:checked') != null && document.querySelector('input[name="planted"]:checked') != null) {
+            $('#submit-div').show();
+        }
+    });
+});
+
+const siteStatForm = $('#site-stat-form');
+siteStatForm.on('submit', (e) => {
+    e.preventDefault();
+
+    var site = $('#site-selection').val();
+    database.ref("squads/" + localStorage.getItem("squadname") + "/site-data/" + $('#site-stat-map-list').val() + "/site" + site).once('value').then(function(s) {
+        var role = document.querySelector('input[name="role"]:checked').value;
+        var winStatus = document.querySelector('input[name="success"]:checked').value;
+        var plantStatus = document.querySelector('input[name="planted"]:checked').value;
+
+        var roundStatus = role + winStatus;
+        database.ref("squads/" + localStorage.getItem("squadname") + "/site-data/" + $('#site-stat-map-list').val() + "/site" + site + "/" + roundStatus).set(s.val()[roundStatus] + 1);
+
+        if (plantStatus == "yes") {
+            database.ref("squads/" + localStorage.getItem("squadname") + "/site-data/" + $('#site-stat-map-list').val() + "/site" + site + "/p" + roundStatus).set(s.val()["p" + roundStatus] + 1);
+        }
+
+        $('#attack').prop('checked', false);
+        $('#defend').prop('checked', false);
+        $('#win').prop('checked', false);
+        $('#loss').prop('checked', false);
+        $('#yes').prop('checked', false);
+        $('#no').prop('checked', false);
+
+        loadSiteData($('#site-stat-map-list').val(), site);
+
+        $('#submit-div').hide();
+    });
 });
