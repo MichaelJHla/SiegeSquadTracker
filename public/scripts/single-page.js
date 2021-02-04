717126487a517aecd22ea3db0ee97afcd36cc2f2
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-04 13:31:45 -0800

    Basic functionality for the operator-bans page

--- public/scripts/single-page.js
+++ public/scripts/single-page.js
@@ -644,3 +644,44 @@ operatorMapSelect.on('change', function() {
         $('#defense-operator').prop("src", "images/operators/" + s.val().defender + ".svg");
     });
 });
+
+const editOperatorBans = $('#edit-operator-bans');
+editOperatorBans.on('click', function() {
+    $('#edit-operators-button').hide();
+    $('#operator-select-div').show();
+});
+
+const attackOperatorDropdown = $('#attack-operators');
+attackOperatorDropdown.on('change', function() {
+    changeOpImg("attack");
+});
+
+const defenseOperatorDropdown = $('#defense-operators');
+defenseOperatorDropdown.on('change', function() {
+    changeOpImg("defense");
+});
+
+//This function is used to change the image of the operators while the user is editing the bans for a map
+function changeOpImg(role) {
+    $('#' + role + '-operator').attr("src", 'images/operators/' + $('#' + role + '-operators').val() + ".svg");
+    $('#unsaved-changes').show();
+}
+
+const submitOperatorBans = $('#submit-operator-bans');
+submitOperatorBans.on('click', function() {
+
+    var map = $('#operator-map-list').val();
+    var dOp = $('#defense-operators').val();
+    var aOp = $('#attack-operators').val();
+
+    var reference = "squads/" + localStorage.getItem("squadname") + "/operator-bans/" + map + "/";
+
+    //Only update the ban if the dropdown menu has been changed, otherwise leave the operator the same
+    if (aOp != null){ database.ref(reference + "attacker").set(aOp); }
+    if (dOp != null){ database.ref(reference + "defender").set(dOp); }
+
+    $('#operator-select-div').hide();
+    $('#unsaved-changes').hide();
+    $('#operator-display').show();
+    $('#edit-operators-button').show();
+});
