d64f33400940ae399bf2506cca10448a10679194
Michael Jonah Hlastala <jhlastala@outlook.com>
2021-02-05 11:02:36 -0800

    Move firebase configurations to a seperate file

---
+++ public/scripts/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBPKECiN2v3FGsKWXxjJ_M8gFaSfHIFCs8",
+    authDomain: "siegesquadstats.firebaseapp.com",
+    databaseURL: "https://siegesquadstats.firebaseio.com",
+    projectId: "siegesquadstats",
+    storageBucket: "siegesquadstats.appspot.com",
+    messagingSenderId: "425029470216",
+    appId: "1:425029470216:web:646a68d606fbfa5bfa69bd",
+    measurementId: "G-082FGX8WQF"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Get a reference to the authentication service
+var auth = firebase.auth();
--- public/scripts/single-page.js
+++ public/scripts/single-page.js
@@ -19,27 +19,6 @@ const maps = ["bank", "border", "chalet", "clubhouse", "coastline",
                 "consulate", "kafe", "kanal", "oregon", "outback", "skyscraper",
                 "park", "villa"];
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBPKECiN2v3FGsKWXxjJ_M8gFaSfHIFCs8",
-    authDomain: "siegesquadstats.firebaseapp.com",
-    databaseURL: "https://siegesquadstats.firebaseio.com",
-    projectId: "siegesquadstats",
-    storageBucket: "siegesquadstats.appspot.com",
-    messagingSenderId: "425029470216",
-    appId: "1:425029470216:web:646a68d606fbfa5bfa69bd",
-    measurementId: "G-082FGX8WQF"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-var database = firebase.database();
-
-// Get a reference to the authentication service
-var auth = firebase.auth();
-
 //Get the current status of the user's login
 auth.onAuthStateChanged(user => {
     if (user) {
